5903ed489923bcaac40f9a4635c9a9a35bf6871a
ASk <ASk@ASk>
2018-11-05 00:52:42 +0900

    11.05 test중

--- WebContent/js/main/main.js
+++ WebContent/js/main/main.js
@@ -5,14 +5,25 @@ $(function(){
 	
 	$('#kadinet_header').mouseout(function(){
 		$(this).removeClass('is_hover');
+		$('.mm a').removeClass('hover');
 	});
 	
 	
-	$('.mm a , .drop_menu').mouseover(function(){
+	$('.mm a').mouseover(function(){
+		$('.mm a').removeClass('hover');
 		$(this).addClass('hover');
 	});
 	
-	$('.mm a').mouseout(function(){
+	$('.drop_menu').mouseout(function(){
 		$('.mm a').removeClass('hover');
+	});
+	
+	$(window).scroll(function() {
+		var currentTop = $(window).scrollTop();
+		if (currentTop == 0) {
+			$('#kadinet_header').removeClass('is_fixed');
+		} else {
+			$('#kadinet_header').addClass('is_fixed');
+		}
 	});
 });
