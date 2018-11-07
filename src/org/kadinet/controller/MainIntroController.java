f4e6aff5528a2b48ae9003b5bae4e606ac2f7ce0
최승훈 <@221.138.141.62>
2018-11-07 23:53:56 +0900

    11.07 협회소개 페이지 추가

---
+++ src/org/kadinet/controller/MainIntroController.java
@@ -0,0 +1,52 @@
+package org.kadinet.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import org.kadinet.util.HttpUtil;
+
+public class MainIntroController implements Controller {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response, String path)
+			throws ServletException, IOException {
+		if ("/intro/greeting.do".equals(path)) {
+			greeting(request, response);
+			HttpUtil.forward(request, response, "/WEB-INF/views/main/intro/greeting.jsp");
+		} else if ("/intro/history.do".equals(path)) {
+			history(request, response);
+			HttpUtil.forward(request, response, "/WEB-INF/views/main/intro/history.jsp");
+		} else if ("/intro/organization.do".equals(path)) {
+			organization(request, response);
+			HttpUtil.forward(request, response, "/WEB-INF/views/main/intro/organization.jsp");
+		} else if ("/intro/visit.do".equals(path)) {
+			visit(request, response);
+			HttpUtil.forward(request, response, "/WEB-INF/views/main/intro/visit.jsp");
+		}
+
+	}
+
+	private void greeting(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setAttribute("page", "greeting");
+
+	}
+
+	private void history(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("page", "history");
+
+	}
+
+	private void organization(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setAttribute("page", "organization");
+
+	}
+
+	private void visit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("page", "visit");
+	}
+}
